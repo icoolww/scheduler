d09ee4475bb8dbdbf74c083fed923a97260e289f
William <williamwijaya.mail@gmail.com>
2022-05-17 17:50:07 -0700

    Testing the DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,17 +6,37 @@ import classNames from "classnames";
 
 
 export default function DayListItem(props) {
-
+  
+  
+  
+  
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
-    "day-list__item--full": props.spots === 0
+    "day-list__item--full": props.spots === 0,
+    
   });
 
-
+  
+       const formatSpots = function (spot) {
+        let result = "";
+        if (spot === 0) {
+          result = "no spots remaining"
+        }
+        else if (spot === 1) {
+          result = `1 spot remaining`
+        }
+        else {
+          result = `${spot} spots remaining`
+        }
+        return result;
+      }
+  
+  
+  
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 onClick={props.setDay} className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
