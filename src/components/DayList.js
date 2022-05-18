de7b828a4e648bb3c7da8496005ea935484cc679
William <williamwijaya.mail@gmail.com>
2022-05-17 19:36:53 -0700

    Adding Day List

---
+++ src/components/DayList.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import DayListItem from "./DayListItem";
+
+
+export default function DayList (props) {
+
+  const daysListItem = props.days.map ((day) => {
+    return (
+      <DayListItem 
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.selected}
+        setDay={props.setDay}
+      />
+    )
+  })
+
+
+  return (
+  <ul>
+    {daysListItem}
+  </ul>
+  );
+}
+
+
+
+// Within the <DayList> component, map over the days array to return <DayListItem> components as children.
+// Remember to import the <DayListItem> component into <DayList>.
+
+// <li className={dayClass} onClick={() => props.setDay(props.name)}>
+// <h2 onClick={props.setDay} className="text--regular">{props.name}</h2> 
+// <h3 className="text--light">{formatSpots(props.spots)}</h3>
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,14 +8,10 @@ import classNames from "classnames";
 export default function DayListItem(props) {
   
   
-  
-  
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0,
-    
   });
-
   
        const formatSpots = function (spot) {
         let result = "";
@@ -30,8 +26,6 @@ export default function DayListItem(props) {
         }
         return result;
       }
-  
-  
   
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
